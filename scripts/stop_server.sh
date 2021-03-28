d0e812e023818b1ca2a914a21d7aec79a86203c0
rvijayarajan <vijay.martius90@gmail.com>
2021-03-28 19:36:37 +0530

    UpdatingBuildSettings

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-export PATH="/usr/local/bin:/usr/bin:$PATH"
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.amzn2.0.1.x86_64"
+export PATH="/usr/local/bin:/usr/bin:$JAVA_HOME/bin:$PATH"
 /tomcat/apache-tomcat-4.1.40/bin/shutdown.sh
