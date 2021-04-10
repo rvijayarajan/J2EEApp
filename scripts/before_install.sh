85d84b3e19aae6bb2ff6b57cdb1d74cf6b6e373c
rvijayarajan <vijay.martius90@gmail.com>
2021-04-10 14:16:32 +0530

    MovingPermissionChanges

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 rm -r /tomcat/apache-tomcat-4.1.40/webapps/buptlab
 rm /tomcat/apache-tomcat-4.1.40/webapps/buptlab.war
+chmod -R 777 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts
