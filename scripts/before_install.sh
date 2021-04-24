6c104bc506474f7090b6af6066a3cdffeff5e753
rvijayarajan <vijay.martius90@gmail.com>
2021-04-24 12:03:19 +0530

    ChangeDBToPostgres

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 rm -rf /tomcat/apache-tomcat-4.1.40/webapps/buptlab
 rm /tomcat/apache-tomcat-4.1.40/webapps/buptlab.war
-chmod -R 777 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts
--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 chmod -R 777 /tomcat/apache-tomcat-4.1.40
+unzip buptlab.war -d buptlab
